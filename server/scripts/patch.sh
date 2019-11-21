0682f5220833cff8eebd84a128946c53b8ba1e17
蒼時弦也 <elct9620@frost.tw>
2019-11-21 15:06:07 +0800

    Fix server image workdir

--- server/scripts/patch.sh
+++ server/scripts/patch.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
+cd $APP_ROOT
 # TODO: Allow specify language
-patch -p0 < patches/import_csv_data.rb.patch
+patch -p0 < $APP_ROOT/patches/import_csv_data.rb.patch
 
 # Dockerize
-patch -p0 < patches/constants.rb.patch
-patch -p0 < patches/server.patch
+patch -p0 < $APP_ROOT/patches/constants.rb.patch
+patch -p0 < $APP_ROOT/patches/server.patch
